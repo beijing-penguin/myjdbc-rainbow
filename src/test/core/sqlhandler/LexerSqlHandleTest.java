e1be8087687dcaef2f0735dd12d70f0f30c0c2cc
DC <429544557@qq.com>
2016-05-08 19:43:37 +0800

    test

--- src/test/core/sqlhandler/LexerSqlHandleTest.java
+++ src/test/core/sqlhandler/LexerSqlHandleTest.java
@@ -12,7 +12,7 @@ import test.User;
 
 public class LexerSqlHandleTest extends TestCase{
 	/**
-	 * 参数匹配规则说明：
+	 * 用户参数和sql通配符强匹配规则说明：
 	 * 1、handleRequest方法的第二个参数中如果有多个Map或者对象，如果字段名，或者某些key相同，则以这个相同的key最后一次出现的时候的值为准。如下test1案例
 	 * 2、sql语句如果含有'#'通配符，则判定该handleRequest的第二个参数只能 传map或者对象。如果多传了集合或数组类型的参数进去，则根据sql分词的结果判定为准，即sql中有?通配符，则将传入的所有的数组或集合作为最终执行sql所必备的元素。如果sql语句中既有?也有#，则程序抛出异常,即#和?不可同时出现
 	 * 3、还有其他疑问，请查阅源码，或直接提交相关bug和建议到github。
