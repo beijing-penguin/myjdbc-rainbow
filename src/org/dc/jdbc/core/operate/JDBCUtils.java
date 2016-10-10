e3480bab18d8b3e20fd89cb8d8972cd45487ab68
dc <429544557@qq.com>
2016-10-10 14:10:37 +0800

    代码优化

--- src/org/dc/jdbc/core/operate/JDBCUtils.java
+++ src/org/dc/jdbc/core/operate/JDBCUtils.java
@@ -16,7 +16,7 @@ import org.apache.commons.logging.LogFactory;
 
 public class JDBCUtils{
 	private static final Log LOG = LogFactory.getLog(JDBCUtils.class);
-	
+
 	public static void close(AutoCloseable...ac){
 		for (int i = 0; i < ac.length; i++) {
 			AutoCloseable autoClose = ac[i];
@@ -72,7 +72,7 @@ public class JDBCUtils{
 		ResultSetMetaData metaData  = rs.getMetaData();
 		int cols_len = metaData.getColumnCount();
 		while(rs.next()){
-			list.add(JDBCUtils.getMap(rs, metaData, cols_len));
+			list.add(getMap(rs, metaData, cols_len));
 		}
 		return list;
 	}
@@ -85,8 +85,8 @@ public class JDBCUtils{
 	private static Map<?,?> parseSqlResultToMap(ResultSet rs) throws Exception{
 		ResultSetMetaData metaData  = rs.getMetaData();
 		int cols_len = metaData.getColumnCount();
-		
-		return JDBCUtils.getMap(rs, metaData, cols_len);
+
+		return getMap(rs, metaData, cols_len);
 	}
 	/**
 	 * 将sql查询结果转化成对象
@@ -101,7 +101,7 @@ public class JDBCUtils{
 		ResultSetMetaData metaData  = rs.getMetaData();
 		int cols_len = metaData.getColumnCount();
 		while(rs.next()){
-			list.add(JDBCUtils.getObject(rs, metaData, cls, cols_len));
+			list.add(getObject(rs, metaData, cls, cols_len));
 		}
 
 		return list;
@@ -109,7 +109,7 @@ public class JDBCUtils{
 	private static Object parseSqlResultToObject(ResultSet rs,Class<?> cls) throws Exception{
 		ResultSetMetaData metaData  = rs.getMetaData();
 		int cols_len = metaData.getColumnCount();
-		return JDBCUtils.getObject(rs, metaData, cls, cols_len);
+		return getObject(rs, metaData, cls, cols_len);
 	}
 	public static List<Object> parseSqlResultToListBaseType(ResultSet rs) throws Exception{
 		List<Object> list = new ArrayList<Object>();
@@ -119,7 +119,7 @@ public class JDBCUtils{
 			throw new Exception("The number of returned data columns is too many");
 		}
 		while(rs.next()){
-			Object cols_value = JDBCUtils.getValueByObjectType(metaData, rs, 0);
+			Object cols_value = getValueByObjectType(metaData, rs, 0);
 			list.add(cols_value);
 		}
 		return list;
@@ -130,11 +130,11 @@ public class JDBCUtils{
 		if(cols_len>1){
 			throw new Exception("The number of returned data columns is too many");
 		}
-		Object cols_value = JDBCUtils.getValueByObjectType(metaData, rs, 0);
-		
+		Object cols_value = getValueByObjectType(metaData, rs, 0);
+
 		return cols_value;
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public static <T> T  parseSqlResultOne(ResultSet rs, Class<? extends T> cls) throws Exception {
 		int row = 0;
@@ -158,23 +158,23 @@ public class JDBCUtils{
 	public static <T> List<T>  parseSqlResultList(ResultSet rs, Class<? extends T> cls) throws Exception {
 		rs.last();
 		int rowNum = rs.getRow();
-		if(rowNum>0){
-			rs.beforeFirst();
-			
-			if(cls==null || Map.class.isAssignableFrom(cls)){//封装成Map
-				return (List<T>) JDBCUtils.parseSqlResultToListMap(rs);
-			}else{
-				if(cls.getClassLoader()==null){//封装成基本类型
-					return (List<T>) JDBCUtils.parseSqlResultToListBaseType(rs);
-				}else{//对象
-					return (List<T>) JDBCUtils.parseSqlResultToListObject(rs,cls);
-				}
+		if(rowNum == 0){
+			return null;
+		}
+		
+		rs.beforeFirst();
+		if(cls==null || Map.class.isAssignableFrom(cls)){//封装成Map
+			return (List<T>) parseSqlResultToListMap(rs);
+		}else{
+			if(cls.getClassLoader()==null){//封装成基本类型
+				return (List<T>) parseSqlResultToListBaseType(rs);
+			}else{//对象
+				return (List<T>) parseSqlResultToListObject(rs,cls);
 			}
 		}
-		return null;
 	}
-	
-	
+
+
 	public static void setParams(PreparedStatement ps, Object[] params) throws Exception {
 		for (int i = 0; i < params.length; i++) {
 			ps.setObject(i+1, params[i]);
@@ -190,7 +190,7 @@ public class JDBCUtils{
 			}catch (Exception e) {
 			}
 			if(field!=null){
-				Object cols_value =  JDBCUtils.getValueByObjectType(metaData, rs, i);
+				Object cols_value =  getValueByObjectType(metaData, rs, i);
 
 				field.setAccessible(true);
 				field.set(obj_newInsten, cols_value);
@@ -200,10 +200,10 @@ public class JDBCUtils{
 	}
 	private static Map<?,?> getMap(ResultSet rs,ResultSetMetaData metaData,int cols_len) throws Exception{
 		Map<String, Object> map = new LinkedHashMap<String, Object>();
-		
+
 		for(int i=0; i<cols_len; i++){
 			String cols_name = metaData.getColumnLabel(i+1);  
-			Object cols_value = JDBCUtils.getValueByObjectType(metaData, rs, i);
+			Object cols_value = getValueByObjectType(metaData, rs, i);
 			map.put(cols_name, cols_value);
 		}
 		return map;
