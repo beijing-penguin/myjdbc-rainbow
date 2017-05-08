b67c8eb205df42980aff8a28185f2c60bef51f75
dc <429544557@qq.com>
2017-05-08 15:32:41 +0800

    更新

--- myjdbc-example/src/org/dc/jdbc/example/MainTest.java
+++ myjdbc-example/src/org/dc/jdbc/example/MainTest.java
@@ -1,5 +1,31 @@
 package org.dc.jdbc.example;
 
-public class MainTest {
+import org.dc.jdbc.core.ConnectionManager;
+import org.dc.jdbc.core.DBHelper;
+import org.dc.jdbc.example.entity.User;
 
+import com.alibaba.fastjson.JSON;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class MainTest {
+	public static void main(String[] args) {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/myjdbc_test?useUnicode=true&characterEncoding=UTF-8&autoReconnect=true&useSSL=false");
+		dataSource.setUsername("root");
+		dataSource.setPassword("123456");
+		
+		DBHelper testDbHelper = new DBHelper(dataSource);
+		ConnectionManager.setTransaction(true);//设置开启事务
+		try {
+			User user = testDbHelper.selectOne("select * from user where id = ? and real_name = ?",User.class,3,"dc");
+			System.out.println(JSON.toJSONString(user));
+			
+			ConnectionManager.commitAll();
+		} catch (Exception e) {
+			e.printStackTrace();
+			ConnectionManager.rollbackAll();
+		}finally {
+			ConnectionManager.closeConnectionAll();
+		}
+	}
 }
---
+++ myjdbc-example/src/org/dc/jdbc/example/entity/User.java
@@ -0,0 +1,42 @@
+package org.dc.jdbc.example.entity;
+
+import java.util.Date;
+
+public class User {
+	private Integer id;
+	private String realName;
+	private Integer sex;
+	private Integer age;
+	private Date createTime;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getRealName() {
+		return realName;
+	}
+	public void setRealName(String realName) {
+		this.realName = realName;
+	}
+	public Integer getSex() {
+		return sex;
+	}
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	
+}
