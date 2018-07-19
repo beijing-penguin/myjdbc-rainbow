5414323fdec11718b8aa094bd011d7cc046dd7ab
echo企鹅 <429544557@qq.com>
2018-07-19 23:48:06 +0800

    commit

--- myjdbc-core/src/org/dc/jdbc/core/pojo/DBType.java
+++
@@ -1,5 +0,0 @@
-package org.dc.jdbc.core.pojo;
-
-public enum DBType {
-	MYSQL,ORACLE,UNKNOW;
-}
--- myjdbc-core/src/org/dc/jdbc/core/pojo/SqlType.java
+++
@@ -1,5 +0,0 @@
-package org.dc.jdbc.core.pojo;
-
-public enum SqlType {
-	SELECT,UPDATE,INSERT,DELETE,UNKNOW;
-}
